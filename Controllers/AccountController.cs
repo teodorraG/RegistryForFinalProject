8f5993c286c918e0f90c4f4667859930743293df
Teodora <teodora_gerasimova@abv.bg>
2020-03-09 22:38:48 +0200

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RegistryForFinalProject.Models.ViewModels;
+
+namespace RegistryForFinalProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(AccountViewModel accViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("LogIn");
+            }
+            return View(accViewModel);
+
+        }
+    }
+}
---
+++ Models/ViewModels/AccountViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistryForFinalProject.Models.ViewModels
+{
+    public class AccountViewModel
+    {
+        [MinLength(5, ErrorMessage = "The username must be between 5 and 15 characters")]
+        [MaxLength(15, ErrorMessage = "The username must be between 5 and 15 characters")]
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression(pattern: @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$", ErrorMessage = "Password must contain at least one: lower case letter, upper case letter and number")]
+        public string Password { get; set; }
+
+        [NotMapped]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
