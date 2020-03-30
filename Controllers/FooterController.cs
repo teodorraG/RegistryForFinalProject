d25dd3405da5d7665f84ac0b6e41f5245576c8c7
Teodora <teodora_gerasimova@abv.bg>
2020-03-30 23:37:11 +0300

    added Terms, Policy and Contact in footer, new footer alignment

---
+++ Controllers/FooterController.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace RegistryForFinalProject.Controllers
+{
+    public class FooterController : Controller
+    {
+        public IActionResult Privacy()
+        {
+            return View("FooterPrivacy");
+        }
+
+        public IActionResult Terms()
+        {
+            return View("FooterTerms");
+        }
+
+        public IActionResult Contact()
+        {
+            return View("FooterContact");
+        }
+    }
+}
